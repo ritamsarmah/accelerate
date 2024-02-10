0854d450180e7ab6980122c353e66d4c8eb98e31
Ritam Sarmah <sarmah.ritam@gmail.com>
2024-02-10 13:20:50 -0800

    Update iOS links

--- iOS/Accelerate/Views/AboutView.swift
+++ iOS/Accelerate/Views/AboutView.swift
@@ -34,9 +34,10 @@ struct AboutView: View {
             .listRowBackground(Color.clear)
 
             Section {
-                Link("Contact Support", destination: URL(string: "mailto:hello@ritam.me")!)
+                Link("Help", destination: URL(string: "https://ritam.me/projects/accelerate/faq-ios")!)
                 Link("Privacy Policy", destination: URL(string: "https://ritam.me/projects/accelerate/privacy")!)
-                Link("FAQ", destination: URL(string: "https://ritam.me/projects/accelerate/faq-ios")!)
+                Link("Submit Feedback", destination: URL(string: "https://github.com/ritamsarmah/accelerate/issues")!)
+                Link("Contact Support", destination: URL(string: "mailto:hello@ritam.me")!)
             }
 
             Section {
